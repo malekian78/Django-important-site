5b122a193c2767e7c95fc4614e3c0b1118f86618
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2025-07-20 18:27:48 +0330

    ❌first try for music input but not accepted

--- homePage/admin.py
+++ homePage/admin.py
@@ -2,15 +2,26 @@
 
 from django.contrib import admin
 from django.http import HttpResponseRedirect
-from .models import HomePage
+from .models import HomePage, MenuItem
+
+class MenuItemInline(admin.TabularInline):
+    model = MenuItem
+    extra = 1
 
 @admin.register(HomePage)
 class HomePageAdmin(admin.ModelAdmin):
+    inlines = [MenuItemInline]
+    
+    def has_delete_permission(self, request, obj=None):
+        # Prevent deletion entirely
+        return False
+
     def has_add_permission(self, request):
-        # Prevent adding new objects if one already exists
+        from .models import HomePage
         return not HomePage.objects.exists()
 
     def changelist_view(self, request, extra_context=None):
+        from .models import HomePage
         obj = HomePage.objects.first()
         if obj:
             return HttpResponseRedirect(f'/admin/homePage/homepage/{obj.id}/change/')
--- homePage/models.py
+++ homePage/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class HomePage(models.Model):
     main_image = models.ImageField(upload_to='homepage/')
     main_description = models.TextField()
@@ -10,3 +11,12 @@ class HomePage(models.Model):
     class Meta:
         verbose_name = "Home Page"
         verbose_name_plural = "Home Page"
+
+class MenuItem(models.Model):
+    homepage = models.ForeignKey(HomePage, related_name='menu_items', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    relative_url = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
