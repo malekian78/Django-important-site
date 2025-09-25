5a9d23898062df919a6f319d743a5791037ad5ec
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2025-09-25 10:13:44 +0330

    تقریبا تا نصفه تمام کردن لیست سخنرانی ها

--- speech/models.py
+++ speech/models.py
@@ -4,13 +4,12 @@ from utils.base_model import BaseModel
 from django.utils.translation import gettext_lazy as _
 from django_jalali.db import models as jmodels
 
-# Create your models here.
+
 # Create your models here.
 class Speech(BaseModel):
     title = models.CharField(verbose_name=_("speech title"), max_length=100)
     image = models.ImageField(upload_to='speech/', default='speech/default.jpg')
     audio_file = models.FileField(blank=True,null=True)
-    # publish_time = jmodels.jDateField(verbose_name=_("publish date"))
     
     class Meta:
         verbose_name = _("speech")
--- speech/views.py
+++ speech/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Speech
 
 # Create your views here.
 def speech_list(request):
-    paginator= Paginator(Speech.objects.all(),1)
-    page_number = request.GET.get('page')
+    posts= Paginator(Speech.objects.all(),2)
+    # page_number = request.GET.get('page')
     # page_number = 1
-    page_obj = paginator.get_page(page_number)
-    context={"speech_list_object":page_obj}
+    # page_obj = paginator.get_page(page_number)
+    # context={"speech_list_object":page_obj}
+    
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
+        
+    context={"posts": posts}
+        
     return render(request, 'speechList.html', context)
